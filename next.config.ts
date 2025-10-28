4551b73b6903d95fda3f059b16ce008e66f324bf
Treston Slater <treston@slatermediacompany.com>
2025-10-27 17:26:53 -0700

    fix root page route for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["djethkxabnuydbbnbsgn.supabase.co"], // ✅ allows Supabase-hosted images/videos
+  },
 };
 
 export default nextConfig;
